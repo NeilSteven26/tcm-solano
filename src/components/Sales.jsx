2627d69059765a945384a448f7da4994f03368a2
NeilSteven26 <cariaganeil08@gmail.com>
2024-09-24 21:50:16 +0800

    edit

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -1,4 +1,4 @@
-import tristantcm from "../assets/tristantcm.jpg";
+import tristanSE from "../assets/tristanSE.png";
 import { motion } from "framer-motion";
 const Sales = () => {
     return (
@@ -10,13 +10,13 @@ const Sales = () => {
                 className="my-5 md:my-10 lg:my-20 text-center text-2xl lg:text-4xl text-neutral-800 font-black">SALES
 
             </motion.h1>
-            <motion.div className="relative"
+            <motion.div className=""
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: 300 }}
                 transition={{ duration: 0.5 }}>
-                <img className="w-full" src={tristantcm} alt="" />
+                <img className="w-[20rem] md:w-[30rem] lg:w-[40rem]" src={tristanSE} alt="" />
                 <div
-                    className="absolute bottom-2 right-3 flex items-center">
+                    className=" bottom-2 right-3 flex items-center justify-center">
                     <div className="flex flex-col items-center lg:gap-3 mb-0 md:mb-1 lg:mb-2 text-[.9rem] md:text-[1rem] lg:text-[3rem] text-neutral-950 font-bold  leading-none">
                         <span >ARJAY P. SERIDON</span>
                         <span>SALES EXECUTIVE</span>
@@ -28,7 +28,7 @@ const Sales = () => {
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, y: -100 }}
                 transition={{ duration: 0.5 }}
-                className="flex flex-col gap-5 my-10 font-bold text-md lg:text-2xl text-neutral-800 indent-10 text-justify ">
+                className="flex flex-col gap-5 my-10 font-bold text-sm md:text-xl lg:text-2xl text-neutral-800 indent-10 text-justify ">
                 <span>
                     At TRANSCYCLE Solano, we offer a wide range of brand-new and repossessed motorcycles at affordable prices, giving you flexible options to suit your budget. Whether you prefer to pay in cash or choose an installment plan, we have financing options available to make owning your dream motorcycle easier and more convenient. Our commitment is to provide quality motorcycles along with exceptional customer service, ensuring a smooth and hassle-free buying experience.
                 </span>
